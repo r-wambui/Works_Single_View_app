8450e6cbec6ecf2bf7dca2d182867eabf05329ed
r-wambui <rose.wambui780@gmail.com>
2021-03-18 14:50:05 +0300

    [Chore]Correct pep 8 errors

--- music_works/management/commands/music_works.py
+++ music_works/management/commands/music_works.py
@@ -9,20 +9,25 @@ class Command(BaseCommand):
     help = "Import music metadata csv"
 
     def handle(self, *args, **options):
-        with open ("data/works_metadata.csv") as music_file:
+        with open("data/works_metadata.csv") as music_file:
             music_metadata = csv.DictReader(music_file)
             for row in music_metadata:
                 if row["iswc"]:
-                    object_retrieved_or_created, created = MusicWorks.objects.get_or_create(iswc=row["iswc"],
-                                                    defaults={"title": row["title"], 
-                                                            "contributors": row["contributors"].split("|")})
+                    music_object, created = MusicWorks.objects.get_or_create(
+                                        iswc=row["iswc"],
+                                        defaults={"title": row["title"],
+                                                "contributors": row["contributors"].split("|")})
                     if not created:
-                        object_retrieved_or_created.contributors = list(set().union(object_retrieved_or_created.contributors, row["contributors"].split("|")))
-                        object_retrieved_or_created.save()
-    
+                        music_object.contributors = list(set().union(
+                                            music_object.contributors,
+                                            row["contributors"].split("|")))
+                        music_object.save()
+
                 else:
-                    search_query = MusicWorks.objects.filter(Q(title=row["title"]) & Q(contributors__overlap=row["contributors"].split("|"))).first()
+                    search_query = MusicWorks.objects.filter(Q(title=row["title"]) & Q(
+                        contributors__overlap=row["contributors"].split("|"))).first()
                     if search_query:
-                        search_query.contributors = list(set().union(search_query.contributors, row["contributors"].split("|")))
+                        search_query.contributors = list(set().union(
+                            search_query.contributors,
+                            row["contributors"].split("|")))
                         search_query.save()
-    
