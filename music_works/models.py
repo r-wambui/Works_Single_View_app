01b2d17eb0a35edb5bf95863ab6de11ad82c9bec
r-wambui <rose.wambui780@gmail.com>
2021-03-19 21:06:50 +0300

    Include timestamps in models

--- music_works/models.py
+++ music_works/models.py
@@ -1,15 +1,15 @@
-# from django.contrib.postgres.fields import ArrayField
 from django.db import models
+from model_utils.models import TimeStampedModel
 
 
-class Contributor(models.Model):
+class Contributor(TimeStampedModel):
     contributor = models.CharField(max_length=200)
 
     def __str__(self):
         return self.contributor
 
 
-class MusicWorks(models.Model):
+class MusicWorks(TimeStampedModel):
     iswc = models.CharField(max_length=50, unique=True, null=True)
     title = models.CharField(max_length=200)
     contributors = models.ManyToManyField(Contributor)
